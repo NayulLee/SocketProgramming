1c0a9f396a3416db275bf926e4204240aff1464c
NayulLee <ahrvhrmsghk@gmail.com>
2025-08-27 02:10:50 +0900

    연습문제02

---
+++ Chapter_01/Ex02.cpp
@@ -0,0 +1,26 @@
+#include "Common.h"
+#pragma comment(lib, "ws2_32.lib")
+
+int main(int argc, char* argv[])
+{
+	// 윈속 초기화
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+	{
+		err_quit("[오류] : 윈속 초기화 실패");
+		return 1;
+	}
+	printf("[알림] 윈속 초기화 성공\n");
+
+	// 소켓 생성
+	SOCKET sock = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sock == INVALID_SOCKET) err_quit("socket()");
+	printf("[알림] : 소켓 생성 성공 \n");
+
+	// 소켓 닫기
+	closesocket(sock);
+
+	// 윈속 종료
+	WSACleanup();
+	return 0;
+}

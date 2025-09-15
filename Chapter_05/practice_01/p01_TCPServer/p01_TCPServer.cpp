2cc0c3827a870a3b2fdc8878e06c705ebf80969c
NayulLee <144967424+NayulLee@users.noreply.github.com>
2025-09-15 17:50:59 +0900

    연습문제1 작성중

---
+++ Chapter_05/practice_01/p01_TCPServer/p01_TCPServer.cpp
@@ -0,0 +1,11 @@
+#include "../../../Chapter_01/Common.h"
+#include <WS2tcpip.h>
+#include <string>
+#include <vector>
+
+int main(int argc, char* argv[])
+{
+	std::string chatID;
+	if (argc > 1) chatID = argv[1];
+	else chatID = "Server" // ±âº»°ª
+}

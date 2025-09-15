8b9491ee7541e079d7086298e7f26f733a872556
NayulLee <144967424+NayulLee@users.noreply.github.com>
2025-09-15 10:34:05 +0900

    고정+가변길이 데이터 전송

---
+++ Chapter_05/TCPServer_FixedVariable/TCPServer_FixedVariable.cpp
@@ -0,0 +1,49 @@
+#include "../../Chapter_01/Common.h"
+#include <WS2tcpip.h>
+
+#define SERVERPORT 9000
+#define BUFSIZE 512
+
+int main(int argc, char* argv[])
+{
+	int retval;
+
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		return 1;
+
+	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (listen_sock == INVALID_SOCKET)	err_quit("socket()");
+
+	// bind()
+	struct sockaddr_in serveraddr;
+	memset(&serveraddr, 0, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons(SERVERPORT);
+	retval = bind(listen_sock, (struct sockaddr*)(&serveraddr), sizeof(serveraddr));
+	if (retval == SOCKET_ERROR) err_quit("bind()"); 
+
+	// listen()
+	retval = listen(listen_sock, SOMAXCONN);
+	if (retval == SOCKET_ERROR) err_quit("listen()");
+
+	// 데이터 통신에 사용할 변수
+	SOCKET client_sock;
+	struct sockaddr_in clientaddr;
+	int addrlen;
+	int len;	// 고정 길이 데이터
+	char buf[BUFSIZE + 1];	// 가변 길이 데이터
+
+	while (1)
+	{
+		// accept()
+		addrlen = sizeof(clientaddr);
+		client_sock = accept(listen_sock, (struct sockaddr*)&clientaddr, &addrlen);
+		if (client_sock == INVALID_SOCKET)
+		{
+			err_display("accept()");
+			break;
+		}
+	}
+}

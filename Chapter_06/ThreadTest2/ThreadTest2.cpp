b1fb710c8527d007c9d739eb624be94816da6c51
NayulLee <144967424+NayulLee@users.noreply.github.com>
2025-09-22 16:11:45 +0900

    실습문제 6-2

---
+++ Chapter_06/ThreadTest2/ThreadTest2.cpp
@@ -0,0 +1,36 @@
+#include <Windows.h>
+#include <iostream>
+
+DWORD WINAPI MyThread(LPVOID arg)
+{
+	while (1);
+	return 0;
+}
+
+int main()
+{
+	// 우선순위 값의 범위를 출력한다.
+	std::cout << "우선순위 : " << THREAD_PRIORITY_IDLE << " ~ "
+		<< THREAD_PRIORITY_TIME_CRITICAL << std::endl;
+
+	// CPU개수를 알아낸다.
+	SYSTEM_INFO si;
+	GetSystemInfo(&si);
+
+	// CPU개수만큼 스레드를 생성한다.
+	for (int i = 0; i < (int)si.dwNumberOfProcessors; i++)
+	{
+		// 스레드를 생성한다
+		HANDLE hThread = CreateThread(NULL, 0, MyThread, NULL, 0, NULL);
+		// 우선순위를 높게 설정한다.
+		SetThreadPriority(hThread, THREAD_PRIORITY_ABOVE_NORMAL);
+		CloseHandle(hThread);
+	}
+
+	// 우선순위를 낮게 설정한다.
+	SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_BELOW_NORMAL);
+	Sleep(1000);
+	std::cout << "주 스레드 실행! " << std::endl;
+
+	return 0;
+}

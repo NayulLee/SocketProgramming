dc62c9b7dbc47f86b501bee4008e4f1e908e8c80
NayulLee <ahrvhrmsghk@gmail.com>
2025-09-26 14:57:11 +0900

    스레드 실행 제어, 종료 연습

---
+++ Chapter_06/ThreadTest3/ThreadTest3.cpp
@@ -0,0 +1,30 @@
+#include <Windows.h>
+#include <iostream>
+
+int sum = 0;
+
+DWORD WINAPI MyThread(LPVOID arg)
+{
+	int num = (int)(long long)arg;
+	for (int i = 1; i <= num; i++)
+		sum += i;
+	return 0;
+}
+
+int main(int argc, char* argv[])
+{
+	int num = 100;
+	HANDLE hThread = CreateThread(
+		NULL, 0, MyThread, (LPVOID)(long long)num, CREATE_SUSPENDED, NULL);
+
+	std::cout << "스레드 실행 전, 계산 결과" << sum << std::endl;
+
+	ResumeThread(hThread); // 일시적정지 상태였던 스레드를 실제로 실행시킨다.
+	WaitForSingleObject(hThread, INFINITE); // 스레드가 끝날 때까지 계속 기다린다.
+
+	std::cout << "스레드 실행 후, 계산 결과" << sum << std::endl;
+
+	CloseHandle(hThread);
+	return 0;
+	
+}
